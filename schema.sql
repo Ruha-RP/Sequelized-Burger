8e377b6b5a3bc3a4b0cc0fc9d42eb948c09a1c3f
Ruha Peera <ruha.rizwan@gmail.com>
2018-01-25 23:17:55 -0500

    modifies schema.sql

--- schema.sql
+++ schema.sql
@@ -1,23 +1,6 @@
--- Creates new database
-CREATE DATABASE burgers_db;
--- Use the database
-USE burgers_db;
--- Create table 
-CREATE TABLE burgers (
--- Add columns
-	id INTEGER(11) AUTO_INCREMENT NOT NULL,
-	burger_name VARCHAR(100),
-	devoured BOOLEAN false DEFAULT,
-	PRIMARY KEY(id)
-);
---Uses the burgers_db
-USE burgers_db;
---Adding data to rows
-INSERT INTO burgers (burger_name)
-VALUES ("Grilled Chicken Burger");
+--This file contains mysql code for creating a new database
 
-INSERT INTO burgers (burger_name)
-VALUES ("Classic Beef Burger");
-
-INSERT INTO burgers (burger_name)
-VALUES ("Cheeseburger")
+--Deltes database if already exist
+DROP DATABASE IF EXISTS sequelizedburger_db;
+-- Creates the "sequelizedburger_db" database --
+CREATE DATABASE sequelizedburger_db
